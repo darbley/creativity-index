0d0f1f194bf33fd1fadeafbe5f1803e28b80b27b
Brad Stapleton <brad@rethinkcanada.com>
2020-01-22 17:16:10 -0500

    add Contact page

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div className="">
+                <h1>Contact</h1>
+            </div>
+        )
+    }
+}
+export default Contact;
