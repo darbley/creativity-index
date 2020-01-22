911b09cf1dfa5df8a10a9b4b65dbe9ea37f12bc7
Brad Stapleton <brad@rethinkcanada.com>
2020-01-22 17:04:32 -0500

    adjust to class component, import cookies, language provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './assets/scss/styles.scss';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
+import LanguageProvider from './providers/LanguageProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    static propTypes = { 
+        cookies: instanceOf(Cookies).isRequired
+    };
+    constructor(){
+        super()
+    }
+    render() {
+        return (
+            <div className="">
+                <h1>The site</h1>
+            </div>
+        )
+    }
 }
-
 export default App;
