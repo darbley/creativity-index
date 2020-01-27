6169251a90bf91182337245c494d7fcd9e18ba54
Brad Stapleton <brad@rethinkcanada.com>
2020-01-27 15:26:33 -0500

    example file for container  folder

---
+++ src/components/example.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Example extends React.Component {
+    render() {
+        return (
+            <div className="xx">
+                
+            </div>
+        )
+    }
+}
+export default Example;
