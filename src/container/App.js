fa52c2afc98e2259687b5706bb4865ae6cebc88c
Steve Lam <steve@rethinkcanada.com>
2020-01-30 16:00:35 -0500

    update app.js

--- src/container/App.js
+++ src/container/App.js
@@ -3,14 +3,22 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import '../assets/scss/styles.scss';
 
 import Home from '../pages/Home/Home';
+import About from '../pages/About/About';
+import Contact from '../pages/Contact/Contact';
+
+import Navbar from '../components/Navbar/Navbar';
+
 
 class App extends React.Component {
   render() {
     return (
         <React.Fragment>
           <Router>
-            <Switch location="">
+            <Switch>
               <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/about" component={About} />
             </Switch>
           </Router>
         </React.Fragment>
