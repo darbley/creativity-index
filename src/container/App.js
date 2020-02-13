22b098a385a423fb4c198c8944ae04c5995178f0
Steve Lam <steve@rethinkcanada.com>
2020-02-13 16:46:04 -0500

    wrapped App in language provider

--- src/container/App.js
+++ src/container/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import '../assets/scss/styles.scss';
 
+import LanguageProvider from '../providers/LanguageProvider';
+
+
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Contact from '../pages/Contact/Contact';
@@ -12,15 +15,18 @@ import Navbar from '../components/Navbar/Navbar';
 class App extends React.Component {
   render() {
     return (
+
         <React.Fragment>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/home" component={Home} />
-              <Route exact path="/contact" component={Contact} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </Router>
+          <LanguageProvider>
+            <Router>
+              <Switch>
+                <Route exact path="/:lang" component={Home} />
+                <Route exact path="/:lang/home" component={Home} />
+                <Route exact path="/:lang/contact" component={Contact} />
+                <Route exact path="/:lang/about" component={About} />
+              </Switch>
+            </Router>
+          </LanguageProvider>
         </React.Fragment>
     )
   }
