fe421b569f07e57ab92ea92e7856fd4dd1aa7515
Brad Stapleton <brad@rethinkcanada.com>
2020-02-27 17:20:28 -0500

    add AnwerOptions component + style

---
+++ src/components/AnswerOptions/AnswerOptions.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './style.scss';
+import PropTypes from 'prop-types';
+
+const AnswerOptions = (props) => {
+    const { answers, onAnswerSelected } = props;
+    const answersList = Object.entries(answers).map(([key,value])=>{
+        return (
+           
+            <li className="answerOption" key={key}>
+                <input
+                    type="radio"
+                    className="radioCustomButton"
+                    name="radioGroup"
+                    checked={false}
+                    value={key}
+                    onChange={(e) => onAnswerSelected(e, key)}
+                />
+                <label className="radioCustomLabel" htmlFor={value}>
+                    {value}
+                </label>
+            </li>
+            
+        )
+    })
+
+    return (
+        <ul className="answer-options">
+            {answersList}
+        </ul>
+    )
+}
+export default AnswerOptions;
+
+AnswerOptions.propTypes = {
+    answers: PropTypes.object.isRequired,
+    onAnswerSelected: PropTypes.func.isRequired
+};
